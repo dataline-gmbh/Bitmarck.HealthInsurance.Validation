98439a2ec338dd5bd2a205fa686dacb4921d2ff4
Mark Junker <m.junker@fubar-dev.de>
2015-05-05 12:16:23 +0200

    Update auf aktuelles IKVM

--- Bitmarck.HealthInsurance.Validation.Windows/build.cmd
+++ Bitmarck.HealthInsurance.Validation.Windows/build.cmd
@@ -1,5 +1,3 @@
 @echo off
-SET IKVM_PATH=%~dp0\..\ikvm-7.2.4630.5
-rem "c:\Program Files (x86)\Java\jdk1.6.0_27\bin\javac.exe" de\perfidia\svcheck\BascTable.java 
-"%IKVM_PATH%\bin\ikvmc.exe" -target:library -version:1.5.0.1 -fileversion:1.5.0.1 "-out:%~dp0\Bitmarck.HealthInsurance.Validation.dll" "%~dp0\..\KernpruefungAAG_1.5.0.1-bin.jar"
-REM de\perfidia\svcheck\BascTable.class
+SET IKVM_PATH=%~dp0\..\ikvm-8.0.5449.1
+"%IKVM_PATH%\bin\ikvmc.exe" -target:library -version:%1 -fileversion:%2 "-out:%~dp0\Bitmarck.HealthInsurance.Validation.dll" "%~dp0\..\KernpruefungAAG_%1-bin.jar"
